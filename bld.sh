16dc319f1f605d3752dddc27fc55e9b954fbe873
Linoff <jlinoff@mbp15-jlinoff.local>
2015-10-03 15:32:42 +0100

    updated 3.1.2 to 3.1.3 to get the most current mpfr release

--- bld.sh
+++ bld.sh
@@ -309,7 +309,7 @@ function my-readlink
 ARS=(
     http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
     https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2
-    http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.bz2
+    http://www.mpfr.org/mpfr-current/mpfr-3.1.3.tar.bz2
     http://www.multiprecision.org/mpc/download/mpc-1.0.2.tar.gz
     http://bugseng.com/products/ppl/download/ftp/releases/1.1/ppl-1.1.tar.bz2
     http://www.bastoul.net/cloog/pages/download/cloog-0.18.1.tar.gz
